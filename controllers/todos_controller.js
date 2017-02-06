350e37de8aa83659ec30e4b81e155e6b7970aead
Ang Cheng Siew <johnacsyen@gmail.com>
2017-02-06 21:16:46 +0800

    submit homework

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -2,10 +2,10 @@ const uuidGenerator = require('uuid/v4')
 const fs = require('fs')
 
 const todos = []
-//// the following line will instead load the todos from a json file when the app starts
+// // the following line will instead load the todos from a json file when the app starts
 // const todos = require('../data.json')
 
-//// The following function can be used to save the todos array to the json data file
+// // The following function can be used to save the todos array to the json data file
 // function save () {
 //   const json = JSON.stringify(todos)
 //   fs.writeFileSync('data.json', json, 'utf8')
@@ -13,22 +13,68 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  if (params.hasOwnProperty('name') && params.name.length > 5) {
+    var todo = {}
+    todo = {
+      _id: uuidGenerator(),
+      name: params.name,
+      description: params.description,
+      completed: params.completed
+    }
+    todos.push(todo)
+    return true
+  } else {
+    return false
+  }
 }
 
 // READ (list & show)
 function list () {
   // return list of all TODOs
+  return todos
 }
 function show (id) {
   // find the TODO with this id
+  for (item in todos) {
+    if (id === todos[item]._id) {
+      return todos[item]
+    }
+  }
+  return null
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
+ if (params.name.length < 5) {
+   return false
+ }
+ for (var i = 0; i < todos.length; i++) {
+   if (todos[i]._id === id) {
+     todos[i].name = params.name
+     todos[i].description = params.description
+     todos[i].completed = params.completed
+     return true
+   }
+ }
+ return false
 }
 
 // DESTROY (destroy & destroyAll)
 function destroy (id) {
+  for (var i = 0; i < todos.length; i++) {
+    if (id === todos[i]._id) {
+      todos.splice(i, 1)
+      return true
+    }
+  }
+  return false
+}
+
+function destroyAll () {
+  for (var i = todos.length - 1; i >= 0; i--) {
+    todos.pop()
+    return true
+  }
 }
 
 module.exports = {
@@ -36,5 +82,6 @@ module.exports = {
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -1,5 +1,70 @@
 const assert = require('assert')
 const todos = require('../controllers/todos_controller.js')
 
+console.log(todos)
 // // Use Assert to Test the functionality of all your CRUD methods e.g.
-// assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+
+var todoItem1 = {
+  name: 'Peters',
+  description: 'Buy bananas',
+  completed: false
+}
+var todoItem2 = {
+  name: 'John',
+  description: 'Sell car',
+  completed: false
+}
+var todoNoNameItem3 = {
+  description: 'Sell house',
+  completed: false
+}
+
+// assert.deepStrictEqual(todos.create({_id: name:,description:,completed:}),{},'parameter provided should be an object')
+
+// testing create function
+assert.strictEqual(todos.create(todoItem1), true, 'Should return true')
+assert.strictEqual(todos.create(todoItem2), false, 'Function create, object not created, name should have at least 5 length')
+assert.strictEqual(todos.create(todoNoNameItem3), false, 'Function create, object not created, no name provided')
+
+// test list function
+assert.ok(todos.list().length > 0, 'Function list should return an array of all todos')
+
+console.log(todos.list())
+var firstObj = todos.list()[0]
+var oneId = todos.list()[0]._id
+console.log(oneId)
+// test show id function
+assert.deepStrictEqual(typeof todos.show(oneId), 'object', 'Function show should return an object  typeof')
+
+assert.deepStrictEqual(todos.show(oneId), firstObj, 'Function show should return an object with the id')
+
+assert.deepStrictEqual(todos.show('da815dba-58a9-4319-8108-64c61742ed99'), null, 'Function show should return  Null if no id found')
+
+// test destroy function
+assert.strictEqual(todos.destroy('12345678'), false, 'Function destroy should return false if no id found')
+console.log(todos.list())
+assert.strictEqual(todos.destroy(oneId), true, 'Function destroy should return true if id found')
+
+// test destroyAll function
+todos.create(todoItem1)
+todos.destroyAll()
+assert.ok(todos.list().length === 0, 'Function destroyAll should return  0 length')
+
+// test update function
+var newObj = {
+  name: 'newName',
+  description: 'Do not do it',
+  completed: false
+}
+
+todos.create(todoItem1)
+console.log(todos.list()[0])
+var newObjId = todos.list()[0]._id
+
+assert.strictEqual(todos.update(newObjId, newObj), true, 'Object with unique id cannot update'
+)
+
+console.log(todos.list()[0])
+
+assert.strictEqual(todos.update(newObjId, todoItem2), false, 'Update parameter name value should be at least 5 characters long.'
+)
